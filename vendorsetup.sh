bec6089eb298a94c04bffb0d953e855ca568960a
Badmaneers <dukebraham24@gmail.com>
2026-07-24 12:19:22 +0530

    even: drop building kernel.
    - Since unification of kernel is done its not worth it to build kernel. We will use the unified prebuild binaries.

--- vendorsetup.sh
+++
@@ -1,3 +0,0 @@
-KT=kernel/realme/even/Makefile
-if ! [ -a $KT ]; then rm -rf kernel/realme/even && git clone -b https://github.com/Badmaneers/zenium_realme_even axion https://github.com/Badmaneers/zenium_realme_even --depth=1 kernel/realme/even
-fi
